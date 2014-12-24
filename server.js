a70e51b0ff591ba402c43d5dc0eb1092eb22a493
fottua <fott.ua@gmail.com>
2014-12-24 22:41:07 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+var app = require('express')();
+var server = require('http').createServer(app);
+var webRTC = require('webrtc.io').listen(server);
+
+var port = process.env.PORT || 8080;
+server.listen(port);
+
+
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+app.get('/style.css', function(req, res) {
+  res.sendfile(__dirname + '/style.css');
+});
+
+app.get('/fullscrean.png', function(req, res) {
+  res.sendfile(__dirname + '/access-media.js');
+});
+
+app.get('/script.js', function(req, res) {
+  res.sendfile(__dirname + '/script.js');
+});
+
+app.get('/webrtc.io.js', function(req, res) {
+  res.sendfile(__dirname + '/browser-api.js');
+});
+
+
